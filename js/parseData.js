cd84117106788e1e8da8a9764a65ab80f1dae749
Matheus Dutra Souza <88992046+MatheusSouza70@users.noreply.github.com>
2022-03-07 19:39:21 -0300

    Add files via upload

---
+++ js/parseData.js
@@ -0,0 +1,43 @@
+function getImages(dados) {
+  console.log(dados["data"]["results"]);
+  let coisasPersonagens = dados["data"]["results"];
+
+  for (let index = 0; index < coisasPersonagens.length; index++) {
+    let primeiro = coisasPersonagens[index];
+    quadrado = document.querySelector("#c" + index + "");
+    quadrado.querySelector("#i" + index + "").src =
+      primeiro["thumbnail"]["path"] + "." + primeiro["thumbnail"]["extension"];
+    quadrado.querySelector("#n" + index + "").textContent =
+      "Nome: " + primeiro["name"];
+    quadrado.querySelector("#cod" + index + "").textContent =
+      "Id: " + primeiro["id"];
+  }
+}
+
+function getHistorys(dados) {
+  //<a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+  content = document.querySelector("#historys");
+  if (dados["data"]["count"] < 0) {
+    //diz que não tem nenhuma historia
+    return;
+  }
+  console.log(dados["data"]);
+  console.log(dados["data"]["results"]);
+
+  dados["data"]["results"].forEach((element) => {
+    title = document.createElement("a");
+    title.classList.add("list-group-item");
+    title.classList.add("list-group-item-action");
+    title.textContent = element["title"];
+    content.appendChild(title);
+  });
+}
+
+$("#exampleModal").on("hidden.bs.modal", function () {
+  console.log("removendo");
+  content = document.querySelector("#historys");
+
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+});
---
+++ js/requests.js
@@ -0,0 +1,68 @@
+const privateKey = "7cd3684824a067744989aa33c44a0fefb24a8740";
+const publicKey = "22e9bab7b462ebbd01fee470d5c30192";
+const maxCharacters = 1500;
+
+function createHash(timeStamp) {
+  const toBeHashed = timeStamp + privateKey + publicKey;
+  const hashedMessage = md5(toBeHashed);
+  return hashedMessage;
+}
+
+function getCharacterList() {
+  //tempo agora
+  const timeStamp = Date.now().toString();
+  //numero randomico de herois
+  const offset = Math.floor(Math.random() * maxCharacters + 1);
+  //hash para validar a requisição
+  const hash = createHash(timeStamp);
+
+  const urlAPI =
+    "http://gateway.marvel.com/v1/public/characters?limit=9&offset=" +
+    offset +
+    "&ts=" +
+    timeStamp +
+    "&apikey=" +
+    publicKey +
+    "&hash=" +
+    hash;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      getImages(data);
+    }
+  };
+  xhttp.open("GET", urlAPI, true);
+  xhttp.send();
+}
+
+function showHistorys(elemento) {
+  const codigo = elemento.parentNode
+    .getElementsByTagName("h5")[1]
+    .textContent.substring(4, 11); //codigo do personagem
+  console.log(codigo);
+  const timeStamp = Date.now().toString(); //tempo agora
+  const hash = createHash(timeStamp); //hash para validar a requisição
+
+  const urlAPI =
+    "https://gateway.marvel.com:443/v1/public/characters/" +
+    codigo +
+    "/stories?ts=" +
+    timeStamp +
+    "&apikey=" +
+    publicKey +
+    "&hash=" +
+    hash;
+  console.log(urlAPI);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      getHistorys(data);
+    }
+  };
+  xhttp.open("GET", urlAPI, true);
+  xhttp.send();
+}
